f3845285e8630232dc6ca22ce39a614c12024657
bruno yuji yoshioka <bruno.yuji1995@gmail.com>
2020-07-17 21:05:26 -0300

    initial commit

---
+++ src/main/java/com/gft/sas/prestadoressaude/PrestadoresSaudeApplication.java
@@ -0,0 +1,13 @@
+package com.gft.sas.prestadoressaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestadoresSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestadoresSaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gft/sas/prestadoressaude/PrestadoresSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.sas.prestadoressaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestadoresSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
